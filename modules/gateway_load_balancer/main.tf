884fc4059c9f0ec6d5d1b2666fec58821729929a
Vidyadhar Evani <41520712+vevani@users.noreply.github.com>
2024-07-10 11:19:50 +0200

    Update main.tf

--- modules/gateway_load_balancer/main.tf
+++ modules/gateway_load_balancer/main.tf
@@ -1,15 +1,63 @@
-resource "azurerm_lb" "external_lb" {
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/lb
+resource "azurerm_lb" "this" {
   name                = var.name
-  location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = "Standard"
+  location            = var.region
+  sku                 = "Gateway"
+  tags                = var.tags
 
   frontend_ip_configuration {
-    name                 = "external-frontend"
-    public_ip_address_id = var.public_ip_id
+    name                          = var.frontend_ip.name
+    private_ip_address_allocation = var.frontend_ip.private_ip_address != null ? "Static" : "Dynamic"
+    private_ip_address_version    = var.frontend_ip.private_ip_address_version
+    private_ip_address            = var.frontend_ip.private_ip_address
+    subnet_id                     = var.frontend_ip.subnet_id
+    zones                         = var.frontend_ip.subnet_id != null ? var.zones : null
   }
 }
 
-output "ip_address" {
-  value = azurerm_lb.external_lb.frontend_ip_configuration[0].public_ip_address
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/lb_backend_address_pool
+resource "azurerm_lb_backend_address_pool" "this" {
+  for_each = var.backends
+
+  name            = each.value.name
+  loadbalancer_id = azurerm_lb.this.id
+
+  dynamic "tunnel_interface" {
+    for_each = each.value.tunnel_interfaces
+    content {
+      identifier = tunnel_interface.value.identifier
+      port       = tunnel_interface.value.port
+      protocol   = tunnel_interface.value.protocol
+      type       = tunnel_interface.value.type
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/lb_probe
+resource "azurerm_lb_probe" "this" {
+  name            = var.health_probe.name
+  loadbalancer_id = azurerm_lb.this.id
+
+  port                = var.health_probe.port
+  protocol            = var.health_probe.protocol
+  probe_threshold     = var.health_probe.probe_threshold
+  request_path        = var.health_probe.protocol != "Tcp" ? var.health_probe.request_path : null
+  interval_in_seconds = var.health_probe.interval_in_seconds
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/lb_rule
+resource "azurerm_lb_rule" "this" {
+  name            = var.lb_rule.name
+  loadbalancer_id = azurerm_lb.this.id
+  probe_id        = azurerm_lb_probe.this.id
+
+  frontend_ip_configuration_name = azurerm_lb.this.frontend_ip_configuration[0].name
+  backend_address_pool_ids       = [for _, v in azurerm_lb_backend_address_pool.this : v.id]
+  load_distribution              = var.lb_rule.load_distribution
+
+  # HA port rule - required by Azure GWLB
+  protocol      = "All"
+  backend_port  = 0
+  frontend_port = 0
 }
