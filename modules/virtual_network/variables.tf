bb9141c650167e93287e53aeebd74beda52d5b5e
vevani <41520712+vevani@users.noreply.github.com>
2024-07-10 10:52:21 +0200

    Create variables.tf

---
+++ modules/virtual_network/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  description = "The name of the virtual network."
+  type        = string
+}
+
+variable "address_space" {
+  description = "The address space of the virtual network."
+  type        = list(string)
+}
+
+variable "location" {
+  description = "The location of the virtual network."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
