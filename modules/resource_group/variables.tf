eb4d65e50aa69280c63683d11e41a9196c3b95a7
vevani <41520712+vevani@users.noreply.github.com>
2024-07-10 10:50:05 +0200

    Create variables.tf

---
+++ modules/resource_group/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the resource group."
+  type        = string
+}
