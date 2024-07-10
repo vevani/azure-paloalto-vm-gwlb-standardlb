9a31127bf32291b2f18a9cb98b066537198437cb
vevani <41520712+vevani@users.noreply.github.com>
2024-07-10 10:53:53 +0200

    Create variables.tf

---
+++ modules/public_ip/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  description = "The name of the public IP address."
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the public IP address."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
